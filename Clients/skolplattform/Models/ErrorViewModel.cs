5f6d590d38d59ed3ff6e71b566fff36bc4f640c2
emil <emil.tornqvist@iths.se>
2022-06-02 19:26:31 +0200

    asd
    asd

--- Clients/skolplattform/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace skolplattform.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
