da063ae5ab1eb0af7da01c558575e861d5757bf1
emil <emil.tornqvist@iths.se>
2022-06-03 08:52:33 +0200

    appsettings

--- Educatcion-API/ViewModels/Categorys/CategoryViewModel.cs
+++ Educatcion-API/ViewModels/Categorys/CategoryViewModel.cs
@@ -4,9 +4,9 @@ namespace Educatcion_API.ViewModels.Categorys
 {
     public class CategoryViewModel
     {
-        [Required]
+    
         public int Id { get; set; }
-        [Required]
+       
         public string? CategoryName { get; set; }
 
     }
